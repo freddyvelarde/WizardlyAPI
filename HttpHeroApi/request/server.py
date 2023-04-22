7e5c24d59f4f3791f32db99491afe6b3c9b9374f
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-22 17:53:56 -0400

    :alien: Creating a simple requester

--- HttpHeroApi/request/server.py
+++ HttpHeroApi/request/server.py
@@ -1,11 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
+
+
+import requests
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
-def index():
+@app.route("/hello", methods=["GET"])
+def hello():
     return jsonify({"message": "Hello, World!"})
+
+
+@app.route("/", methods=["POST"])
+def index():
+    try:
+        body = request.json
+        response = requests.get(body["url"])
+        return jsonify(response.json())
+    except Exception as e:
+        return make_response(jsonify({"message": "error requesting", "error": e}), 200)
 
 
 if __name__ == "__main__":
