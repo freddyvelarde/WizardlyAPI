aad450aabb62b95b5d06406d56492b3de01f5f6b
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-26 21:41:44 -0400

    :white_check_mark: Adding test for removing users

--- wizardlyapi-server/src/tests/endpoints/users.test.ts
+++ wizardlyapi-server/src/tests/endpoints/users.test.ts
@@ -9,7 +9,9 @@ describe("UserControllers", () => {
     password: "12345",
   };
 
-  describe("POST /signup", () => {
+  describe("POST /auth/signup and DELETE /users/remove", () => {
+    let accessToken: string;
+
     it("should create a new user", async () => {
       const response = await request(app)
         .post("/auth/signup")
@@ -20,6 +22,8 @@ describe("UserControllers", () => {
       expect(response.body.data.username).toBe(user.username);
       expect(response.body.data.email).toBe(user.email);
       expect(response.body.message).toBe("User was created successfully");
+
+      accessToken = response.body.token;
     });
 
     it("should return an error if the email is already used", async () => {
@@ -31,8 +35,16 @@ describe("UserControllers", () => {
       expect(response.body.message).toBe("Your email is already used");
     });
 
-    // it("should return an error if the password cannot be hashed", async () => {
-    // });
+    it("Removing user", async () => {
+      const response = await request(app)
+        .delete("/users/remove")
+        .set("access-token", accessToken)
+        .send({ password: user.password });
+
+      expect(response.body.message).toBe("User removed");
+      expect(response.body.status).toBe(200);
+      expect(response.status).toBe(200);
+    });
     //
     // it("should return an error if the JWT token cannot be generated", async () => {
     // });
