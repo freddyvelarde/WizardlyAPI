91806c56e03663c815202a6b1e535ef7237c415e
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-29 18:50:29 -0400

    :recycle: refactoring tests structure.

--- wizardlyapi-server/src/tests/integration-tests/requests.test.ts
+++ wizardlyapi-server/src/tests/integration-tests/requests.test.ts
@@ -1,19 +1,17 @@
 import request from "supertest";
 import App from "../../app";
 
-describe("Testing request controllers and features", () => {
-  const app = new App().app;
+const app = new App().app;
 
-  describe("GET /request", () => {
-    it("should return no requests added yet", async () => {
-      const response = await request(app).get("/request").send().expect(404);
+describe("GET /request", () => {
+  it("should return no requests added yet", async () => {
+    const response = await request(app).get("/request").send().expect(404);
 
-      expect(response.body.message).toBe("There's no requests added yet");
-    });
-    // TODO: it("should return all requests", async () => {})
+    expect(response.body.message).toBe("There's no requests added yet");
   });
+  // TODO: it("should return all requests", async () => {})
+});
 
-  describe("POST /request/create", () => {
-    it("should create a new request", async () => {});
-  });
+describe("POST /request/create", () => {
+  it("should create a new request", async () => {});
 });
--- wizardlyapi-server/src/tests/integration-tests/users.test.ts
+++ wizardlyapi-server/src/tests/integration-tests/users.test.ts
@@ -1,115 +1,113 @@
 import request from "supertest";
 import App from "../../app";
 
-describe("User controllers: Authentication and user features", () => {
-  const app = new App().app;
-  let accessToken: string;
-  const user = {
-    username: "Jhondoe",
-    email: "jhon@email.com",
-    password: "12345",
-  };
+const app = new App().app;
+let accessToken: string;
+const user = {
+  username: "Jhondoe",
+  email: "jhon@email.com",
+  password: "12345",
+};
 
-  describe("POST /auth/signup", () => {
-    it("should create a new user", async () => {
-      const response = await request(app)
-        .post("/auth/signup")
-        .send(user)
-        .expect(200);
+describe("POST /auth/signup", () => {
+  it("should create a new user", async () => {
+    const response = await request(app)
+      .post("/auth/signup")
+      .send(user)
+      .expect(200);
 
-      expect(response.body.token).toBeDefined();
-      expect(response.body.data.username).toBe(user.username);
-      expect(response.body.data.email).toBe(user.email);
-      expect(response.body.message).toBe("User was created successfully");
-    });
-
-    it("should return an error if the email is already used", async () => {
-      const response = await request(app)
-        .post("/auth/signup")
-        .send(user)
-        .expect(409);
-
-      expect(response.body.message).toBe("Your email is already used");
-    });
+    expect(response.body.token).toBeDefined();
+    expect(response.body.data.username).toBe(user.username);
+    expect(response.body.data.email).toBe(user.email);
+    expect(response.body.message).toBe("User was created successfully");
   });
 
-  describe("POST /auth/login", () => {
-    it("should log in the user successfully", async () => {
-      const response = await request(app)
-        .post("/auth/login")
-        .send({ email: user.email, password: user.password })
-        .expect(200);
+  it("should return an error if the email is already used", async () => {
+    const response = await request(app)
+      .post("/auth/signup")
+      .send(user)
+      .expect(409);
 
-      expect(response.body.auth).toBe(true);
-      accessToken = response.body.token;
-    });
+    expect(response.body.message).toBe("Your email is already used");
+  });
+});
 
-    it("should failed for wrong password", async () => {
-      const response = await request(app)
-        .post("/auth/login")
-        .send({ email: user.email, password: "wrongpasswor" })
-        .expect(500);
+describe("POST /auth/login", () => {
+  it("should log in the user successfully", async () => {
+    const response = await request(app)
+      .post("/auth/login")
+      .send({ email: user.email, password: user.password })
+      .expect(200);
 
-      expect(response.body.message).toBe(
-        "You password is not valid, please try again."
-      );
-      expect(response.body.auth).toBe(false);
-    });
-
-    it("should failed for invalid email", async () => {
-      const response = await request(app)
-        .post("/auth/login")
-        .send({ email: "wrongemail@gmail.com", password: "wrongpasswor" })
-        .expect(404);
-
-      expect(response.body.message).toBe("Your email is not valid");
-      expect(response.body.auth).toBe(false);
-    });
+    expect(response.body.auth).toBe(true);
+    accessToken = response.body.token;
   });
 
-  describe("DELETE /users/remove", () => {
-    it("should failed for not access-token", async () => {
-      const response = await request(app)
-        .delete("/users/remove")
-        .send({ password: "wrongpassword" })
-        .expect(500);
+  it("should failed for wrong password", async () => {
+    const response = await request(app)
+      .post("/auth/login")
+      .send({ email: user.email, password: "wrongpasswor" })
+      .expect(500);
 
-      expect(response.body.message).toBe("you don't have any token");
-      expect(response.body.access).toBe(false);
-    });
+    expect(response.body.message).toBe(
+      "You password is not valid, please try again."
+    );
+    expect(response.body.auth).toBe(false);
+  });
 
-    it("should failed for not token valid", async () => {
-      const response = await request(app)
-        .delete("/users/remove")
-        .set("access-token", "notvalidtoken")
-        .send({ password: "wrongpassword" })
-        .expect(200);
+  it("should failed for invalid email", async () => {
+    const response = await request(app)
+      .post("/auth/login")
+      .send({ email: "wrongemail@gmail.com", password: "wrongpasswor" })
+      .expect(404);
 
-      expect(response.body.message).toBe("jwt malformed");
-      expect(response.body.name).toBe("JsonWebTokenError");
-    });
+    expect(response.body.message).toBe("Your email is not valid");
+    expect(response.body.auth).toBe(false);
+  });
+});
 
-    it("should failed for wrong password passed", async () => {
-      const response = await request(app)
-        .delete("/users/remove")
-        .set("access-token", accessToken)
-        .send({ password: "wrongpassword" })
-        .expect(500);
+describe("DELETE /users/remove", () => {
+  it("should failed for not access-token", async () => {
+    const response = await request(app)
+      .delete("/users/remove")
+      .send({ password: "wrongpassword" })
+      .expect(500);
 
-      expect(response.body.message).toBe(
-        "You password is not valid, please try again."
-      );
-    });
+    expect(response.body.message).toBe("you don't have any token");
+    expect(response.body.access).toBe(false);
+  });
 
-    it("Removing user", async () => {
-      const response = await request(app)
-        .delete("/users/remove")
-        .set("access-token", accessToken)
-        .send({ password: user.password });
+  it("should failed for not token valid", async () => {
+    const response = await request(app)
+      .delete("/users/remove")
+      .set("access-token", "notvalidtoken")
+      .send({ password: "wrongpassword" })
+      .expect(200);
 
-      expect(response.body.message).toBe("User removed");
-      expect(response.body.status).toBe(200);
-      expect(response.status).toBe(200);
-    });
+    expect(response.body.message).toBe("jwt malformed");
+    expect(response.body.name).toBe("JsonWebTokenError");
+  });
+
+  it("should failed for wrong password passed", async () => {
+    const response = await request(app)
+      .delete("/users/remove")
+      .set("access-token", accessToken)
+      .send({ password: "wrongpassword" })
+      .expect(500);
+
+    expect(response.body.message).toBe(
+      "You password is not valid, please try again."
+    );
+  });
+
+  it("Removing user", async () => {
+    const response = await request(app)
+      .delete("/users/remove")
+      .set("access-token", accessToken)
+      .send({ password: user.password });
+
+    expect(response.body.message).toBe("User removed");
+    expect(response.body.status).toBe(200);
+    expect(response.status).toBe(200);
   });
 });
