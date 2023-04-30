289129a5bc78ec3a0d9c7aedadae7508d4e5306c
Freddy Velarde <freddyvelarde59@gmail.com>
2023-04-30 09:06:30 -0400

    :test_tube: adding tests for `workshop` service

--- wizardlyapi-server/src/tests/integration-tests/workshop.test.ts
+++ wizardlyapi-server/src/tests/integration-tests/workshop.test.ts
@@ -10,6 +10,14 @@ const user = {
   password: "somepassword",
 };
 
+const newWorkshop = {
+  name: "Wizardly API",
+  description: "This is for my Wizardly development environment",
+  is_public: false,
+};
+
+let workshopId: string;
+
 beforeAll(async () => {
   const response = await request(app).post("/auth/signup").send(user);
 
@@ -40,5 +48,39 @@ describe("GET /workshop: Get all workshops by user", () => {
 
     expect(response.body.message).toBe("No workshop added yet");
     expect(response.body.status).toBe(404);
+  });
+});
+
+describe("POST /workshop/create", () => {
+  it("Should failed for missing access-token", async () => {
+    const response = await request(app)
+      .post("/workshop/create")
+      .send(newWorkshop);
+
+    expect(response.body.message).toBe("you don't have any token");
+    expect(response.body.access).toBe(false);
+  });
+
+  it("Should create a new workshop", async () => {
+    const response = await request(app)
+      .post("/workshop/create")
+      .set("access-token", token)
+      .send(newWorkshop)
+      .expect(200);
+
+    expect(response.body.created).toBe(true);
+    expect(response.body.data.name).toBe(newWorkshop.name);
+    workshopId = response.body.data.id;
+  });
+});
+
+describe("DELETE /workshop/remove", () => {
+  it("Should remove workshop", async () => {
+    const response = await request(app)
+      .delete("/workshop/remove")
+      .send({ id: workshopId })
+      .expect(200);
+
+    expect(response.body.message).toBe("Workshop was removed");
   });
 });
